0f655166cf41547776443d1281ffea1d2ace21ed
Lee Bom <Sbome0214@naver.com7>
2022-08-18 02:10:22 +0900

    STYLE | UI UX 최종

--- FrontEnd/src/components/Reserve/GoToSurvey.vue
+++ FrontEnd/src/components/Reserve/GoToSurvey.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
-    <base-button type="default" class="childbutton" @click="modals.modal1 = true">
+    <base-button
+      outline
+      type="secondary"
+      class="childbutton"
+      @click="modals.modal1 = true"
+    >
       상담사추천
     </base-button>
     <modal :show.sync="modals.modal1">
-      <h6 slot="header" class="modal-title" id="modal-title-default">
-        <h4 class="display-4">
-            <h4>문진표가 작성되지 않았습니다.</h4>
-            <h4>문진표 작성 후 예약해주세요</h4> 
-        </h4>
+      <h6 slot="header" class="modal-title text-left" id="modal-title-default">
+        <h5 class=" pt-md-5 px-md-2">
+          <h5>문진표가 작성되지 않았습니다.</h5>
+          <h5>문진표 작성 후 예약해주세요.</h5>
+        </h5>
       </h6>
       <router-link to="/survey" class="col-lg-6 px-0">
         <base-button type="primary" class="childbutton">문진표작성</base-button>
@@ -19,13 +24,13 @@
 
 <script>
 import Modal from "@/components/Modal.vue";
-import BaseButton from '../BaseButton.vue';
+import BaseButton from "../BaseButton.vue";
 export default {
   components: {
     Modal,
   },
   data() {
-    BaseButton
+    BaseButton;
     return {
       modals: {
         modal1: false,
@@ -36,6 +41,23 @@ export default {
 </script>
 
 <style scoped>
+/* 자녀관리 하단의 문진표보기 등 버튼 */
+.childbutton {
+  margin: 10px;
+  width: 80%;
+  border: 1px solid rgb(237, 237, 237);
+}
+/* 버튼 관련~~~~ */
+
+button:focus,
+button:active {
+  outline: none;
+  box-shadow: none;
+}
+.childbutton:hover {
+  background-color: #ffdcb8;
+  border: 1px solid #dcdcdc;
+}
 .card_about p {
   margin-bottom: 1px;
   color: #000;
--- FrontEnd/src/components/Reserve/ReserveConform.vue
+++ FrontEnd/src/components/Reserve/ReserveConform.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="mt-5 align-items-end">
     <!-- <a href="#/reserv-parent" @click="modals.modal1 = true"> -->
-      <div class="card_about text-center">
-        <base-button class="reserve_confirm" block type="primary" @click="reserve"
-          >예약하기</base-button
-        >
-      </div>
+    <div class="card_about text-center">
+      <base-button class="reserve_confirm" block type="primary" @click="reserve"
+        >예약하기</base-button
+      >
+    </div>
     <!-- </a> -->
     <modal :show.sync="modals.modal1">
       <h6 slot="header" class="modal-title" id="modal-title-default">
         <h4 class="display-4">예약이 완료되었습니다.</h4>
       </h6>
       <div class="text-right">
-        <base-button type="link" class="ml-auto" @click="modals.modalFlag = false"
+        <base-button
+          type="link"
+          class="ml-auto"
+          @click="modals.modalFlag = false"
           >닫기
         </base-button>
         <router-link to="/childReserveShow"
@@ -25,7 +28,7 @@
 
 <script>
 import Modal from "@/components/Modal.vue";
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   components: {
@@ -38,32 +41,32 @@ export default {
       },
     };
   },
-  props: ['result'],
+  props: ["result"],
   methods: {
     reserve() {
       // console.log(this.$store.state.accounts.userid)
       // console.log(this.$route.params.childInfo.name)
       // console.log(this.$route.params.counselorInfo.thera_id)
-      console.log(this.result)
+      console.log(this.result);
       axios({
-        url: 'https://i7a606.q.ssafy.io/service-api/reserv-parent',
-        method: 'post',
+        url: "https://i7a606.q.ssafy.io/service-api/reserv-parent",
+        method: "post",
         data: {
-          "child_name": this.$route.params.childInfo.name,
-          "parent_id": this.$store.state.accounts.userid,
-          "thera_id": this.$route.params.counselorInfo.thera_id,
-          "reserv_time": this.result
-        }
+          child_name: this.$route.params.childInfo.name,
+          parent_id: this.$store.state.accounts.userid,
+          thera_id: this.$route.params.counselorInfo.thera_id,
+          reserv_time: this.result,
+        },
       })
-        .then(res => {
-          console.log(res.data)
-          this.modals.modalFlag = true
+        .then((res) => {
+          console.log(res.data);
+          this.modals.modalFlag = true;
         })
-        .catch(err => {
-          console.log(err.response)
-        })
-    }
-  }
+        .catch((err) => {
+          console.log(err.response);
+        });
+    },
+  },
 };
 </script>
 
