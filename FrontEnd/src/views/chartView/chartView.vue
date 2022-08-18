eeacea92b91171d4ccc967fd9d2ff7c662a0d365
Lee Bom <Sbome0214@naver.com7>
2022-08-18 16:55:05 +0900

    STYEL | 통계 낸거 배경

--- FrontEnd/src/views/chartView/chartView.vue
+++ FrontEnd/src/views/chartView/chartView.vue
@@ -1,57 +1,65 @@
 <template>
-  <div>
+  <div class="profile-page">
+    <section class="section-profile-cover section-shaped my-0">
+      <div class="shape shape-style-1 shape-primary shape-skew alpha-4"></div>
+    </section>
 
-    <div id="titleDiv">
-      <h1>{{this.childData.childName}} 님의 게임 기록</h1>
-    </div>
+    <section class="section section-skew p-0">
+      <div class="container">
+        <card shadow class="py-5 px-md-5 card-profile mt--300" no-body>
+          <div id="titleDiv">
+            <h1>{{ this.childData.childName }} 님의 게임 기록</h1>
+          </div>
 
-    <div id="baseDiv">
-      <LineChart :childId="this.childData.childId" id="chartDiv" />
-      <base-button id="moveChildrenBtn" @click="moveChildren">뒤로가기</base-button>
-    </div>
-
-    
+          <div id="baseDiv">
+            <LineChart :childId="this.childData.childId" id="chartDiv" />
+            <base-button id="moveChildrenBtn" @click="moveChildren"
+              >뒤로가기</base-button
+            >
+          </div>
+        </card>
+      </div>
+    </section>
   </div>
-
 </template>
 
 <script>
-import LineChart from '@/components/chartComp/Line.vue'
+import LineChart from "@/components/chartComp/Line.vue";
 
 export default {
   methods: {
-    moveChildren () {
-      this.$router.push({name: 'children'})
+    moveChildren() {
+      this.$router.push({ name: "children" });
     },
   },
-  data () {
+  data() {
     return {
-      childData: this.$route.params
-    }
+      childData: this.$route.params,
+    };
   },
   components: {
-    LineChart
-  }
-}
+    LineChart,
+  },
+};
 </script>
 <style scoped>
-#baseDiv{
+#baseDiv {
   display: flex;
   justify-content: center;
 }
 
-#chartDiv{
+#chartDiv {
   margin-top: 10%;
   width: 50%;
   height: 50%;
 }
 
-#moveChildrenBtn{
+#moveChildrenBtn {
   margin-top: 30%;
   height: 5%;
 }
 
-#titleDiv{
+#titleDiv {
   margin-top: 5%;
   display: flex;
   justify-content: center;
