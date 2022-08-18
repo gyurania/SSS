fcf584c6ef3fc4362647e0c848f97eb386f8d7dd
qoqls1212 <qoqls1212@naver.com>
2022-08-18 13:03:39 +0900

    FIX| 문진표&회원가입이메일&프로필사진&자녀관리

--- FrontEnd/src/views/ChildPlusView/ChildListView.vue
+++ FrontEnd/src/views/ChildPlusView/ChildListView.vue
@@ -240,7 +240,7 @@ export default {
         })
           .then((res) => {
             console.log(res.data);
-            router.push({ name: "children" });
+            this.$router.go()
           })
           .catch((err) => {
             console.log(err.response);
